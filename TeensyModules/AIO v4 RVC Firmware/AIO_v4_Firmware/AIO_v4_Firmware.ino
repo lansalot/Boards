385f2883fe730f1de51e27cc5f49d241437a9012
lansalot <lansalot@gmail.com>
2024-07-29 20:44:53 +0100

    Added BETA CANBUS engage to AIO RVC

--- TeensyModules/AIO v4 RVC Firmware/AIO_v4_Firmware/AIO_v4_Firmware.ino
+++ TeensyModules/AIO v4 RVC Firmware/AIO_v4_Firmware/AIO_v4_Firmware.ino
@@ -26,6 +26,8 @@
 
 /************************* User Settings *************************/
 // Serial Ports
+#include "CANBUS.h"
+
 #define SerialAOG Serial                //AgIO USB conection
 #define SerialRTK Serial3               //RTK radio
 HardwareSerial* SerialGPS = &Serial7;   //Main postion receiver (GGA)
@@ -66,14 +68,14 @@ bool useBNO08xRVC = false;
 
 struct ConfigIP
 {
-    uint8_t ipOne = 192;
-    uint8_t ipTwo = 168;
-    uint8_t ipThree = 5;
+	uint8_t ipOne = 192;
+	uint8_t ipTwo = 168;
+	uint8_t ipThree = 5;
 };  ConfigIP networkAddress;   //3 bytes
 
 // IP & MAC address of this module of this module
-byte Eth_myip[4] = { 0, 0, 0, 0}; //This is now set via AgIO
-byte mac[] = {0x00, 0x00, 0x56, 0x00, 0x00, 0x78};
+byte Eth_myip[4] = { 0, 0, 0, 0 }; //This is now set via AgIO
+byte mac[] = { 0x00, 0x00, 0x56, 0x00, 0x00, 0x78 };
 
 unsigned int portMy = 5120;                         // port of this module
 unsigned int AOGNtripPort = 2233;                   // port NTRIP data from AOG comes in
@@ -114,7 +116,7 @@ double rollDual = 0;
 double relPosD = 0;
 double heading = 0;
 
-byte ackPacket[72] = {0xB5, 0x62, 0x01, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+byte ackPacket[72] = { 0xB5, 0x62, 0x01, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 uint8_t GPSrxbuffer[serial_buffer_size];    //Extra serial rx buffer
 uint8_t GPStxbuffer[serial_buffer_size];    //Extra serial tx buffer
@@ -137,185 +139,188 @@ float yaw = 0;
 // Setup procedure ------------------------
 void setup()
 {
-    delay(500);                       //Small delay so serial can monitor start up
-    set_arm_clock(450000000);         //Set CPU speed to 150mhz
-    Serial.print("CPU speed set to: ");
-    Serial.println(F_CPU_ACTUAL);
+	delay(500);                       //Small delay so serial can monitor start up
+	set_arm_clock(450000000);         //Set CPU speed to 150mhz
+	Serial.print("CPU speed set to: ");
+	Serial.println(F_CPU_ACTUAL);
 
-    pinMode(GGAReceivedLED, OUTPUT);
-    pinMode(Power_on_LED, OUTPUT);
-    pinMode(Ethernet_Active_LED, OUTPUT);
-    pinMode(GPSRED_LED, OUTPUT);
-    pinMode(GPSGREEN_LED, OUTPUT);
-    pinMode(AUTOSTEER_STANDBY_LED, OUTPUT);
-    pinMode(AUTOSTEER_ACTIVE_LED, OUTPUT);
-    
-    // the dash means wildcard
-    parser.setErrorHandler(errorHandler);
-    parser.addHandler("G-GGA", GGA_Handler);
-    parser.addHandler("G-VTG", VTG_Handler);
+	pinMode(GGAReceivedLED, OUTPUT);
+	pinMode(Power_on_LED, OUTPUT);
+	pinMode(Ethernet_Active_LED, OUTPUT);
+	pinMode(GPSRED_LED, OUTPUT);
+	pinMode(GPSGREEN_LED, OUTPUT);
+	pinMode(AUTOSTEER_STANDBY_LED, OUTPUT);
+	pinMode(AUTOSTEER_ACTIVE_LED, OUTPUT);
 
-    delay(10);
-    Serial.println("Start setup");
+	// the dash means wildcard
+	parser.setErrorHandler(errorHandler);
+	parser.addHandler("G-GGA", GGA_Handler);
+	parser.addHandler("G-VTG", VTG_Handler);
 
-    SerialGPS->begin(baudGPS);
-    SerialGPS->addMemoryForRead(GPSrxbuffer, serial_buffer_size);
-    SerialGPS->addMemoryForWrite(GPStxbuffer, serial_buffer_size);
+	delay(10);
+	Serial.println("Start setup");
 
-    delay(10);
-    SerialRTK.begin(baudRTK);
-    SerialRTK.addMemoryForRead(RTKrxbuffer, serial_buffer_size);
+	SerialGPS->begin(baudGPS);
+	SerialGPS->addMemoryForRead(GPSrxbuffer, serial_buffer_size);
+	SerialGPS->addMemoryForWrite(GPStxbuffer, serial_buffer_size);
 
-    delay(10);
-    SerialGPS2->begin(baudGPS);
-    SerialGPS2->addMemoryForRead(GPS2rxbuffer, serial_buffer_size);
-    SerialGPS2->addMemoryForWrite(GPS2txbuffer, serial_buffer_size);
+	delay(10);
+	SerialRTK.begin(baudRTK);
+	SerialRTK.addMemoryForRead(RTKrxbuffer, serial_buffer_size);
 
-    Serial.println("SerialAOG, SerialRTK, SerialGPS and SerialGPS2 initialized");
+	delay(10);
+	SerialGPS2->begin(baudGPS);
+	SerialGPS2->addMemoryForRead(GPS2rxbuffer, serial_buffer_size);
+	SerialGPS2->addMemoryForWrite(GPS2txbuffer, serial_buffer_size);
 
-    Serial.println("\r\nStarting AutoSteer...");
-    autosteerSetup();
-  
-    Serial.println("\r\nStarting Ethernet...");
-    EthernetStart();
+	Serial.println("SerialAOG, SerialRTK, SerialGPS and SerialGPS2 initialized");
 
-    SerialIMU->begin(115200);
-    rvc.begin(SerialIMU);
+	Serial.println("\r\nStarting AutoSteer...");
+	autosteerSetup();
 
-    static elapsedMillis rvcBnoTimer = 0;
-    Serial.println("\r\nChecking for serial BNO08x");
-    while (rvcBnoTimer < 1000)
-    {
-        //check if new bnoData
-        if (rvc.read(&bnoData))
-        {
-            useBNO08xRVC = true;
-            Serial.println("Serial BNO08x Good To Go :-)");
-            imuHandler();
-            break;
-        }
-    }
-    if (!useBNO08xRVC)  Serial.println("No Serial BNO08x not Connected or Found");
+	Serial.println("\r\nStarting Ethernet...");
+	EthernetStart();
 
-  Serial.println("\r\nEnd setup, waiting for GPS...\r\n");
+	SerialIMU->begin(115200);
+	rvc.begin(SerialIMU);
+
+	static elapsedMillis rvcBnoTimer = 0;
+	Serial.println("\r\nChecking for serial BNO08x");
+	while (rvcBnoTimer < 1000)
+	{
+		//check if new bnoData
+		if (rvc.read(&bnoData))
+		{
+			useBNO08xRVC = true;
+			Serial.println("Serial BNO08x Good To Go :-)");
+			imuHandler();
+			break;
+		}
+	}
+	if (!useBNO08xRVC)  Serial.println("No Serial BNO08x not Connected or Found");
+
+	Serial.println("Right... time for some CANBUS!");
+	CAN_Setup();
+	Serial.println("\r\nEnd setup, waiting for GPS...\r\n");
 }
 
 void loop()
 {
-    // Read incoming nmea from GPS
-    if (SerialGPS->available())
-    {
-        parser << SerialGPS->read();
-    }
+	CANBUS_Receive();
+	// Read incoming nmea from GPS
+	if (SerialGPS->available())
+	{
+		parser << SerialGPS->read();
+	}
 
-    // Check for RTK via Radio
-    if (SerialRTK.available())
-    {
-        SerialGPS->write(SerialRTK.read());
-    }
+	// Check for RTK via Radio
+	if (SerialRTK.available())
+	{
+		SerialGPS->write(SerialRTK.read());
+	}
 
-    // Check for RTK via UDP
-    unsigned int packetLength = Eth_udpNtrip.parsePacket();
+	// Check for RTK via UDP
+	unsigned int packetLength = Eth_udpNtrip.parsePacket();
 
-    if (packetLength > 0)
-    {
-        if (packetLength > serial_buffer_size) packetLength = serial_buffer_size;
-        Eth_udpNtrip.read(Eth_NTRIP_packetBuffer, packetLength);
-        SerialGPS->write(Eth_NTRIP_packetBuffer, packetLength);
-    }
+	if (packetLength > 0)
+	{
+		if (packetLength > serial_buffer_size) packetLength = serial_buffer_size;
+		Eth_udpNtrip.read(Eth_NTRIP_packetBuffer, packetLength);
+		SerialGPS->write(Eth_NTRIP_packetBuffer, packetLength);
+	}
 
-    // If both dual messages are ready, send to AgOpen
-    if (dualReadyGGA == true && dualReadyRelPos == true)
-    {
-        BuildNmea();
-        dualReadyGGA = false;
-        dualReadyRelPos = false;
-    }
+	// If both dual messages are ready, send to AgOpen
+	if (dualReadyGGA == true && dualReadyRelPos == true)
+	{
+		BuildNmea();
+		dualReadyGGA = false;
+		dualReadyRelPos = false;
+	}
 
-    // If anything comes in SerialGPS2 RelPos data
-    if (SerialGPS2->available())
-    {
-        uint8_t incoming_char = SerialGPS2->read();  //Read RELPOSNED from F9P
+	// If anything comes in SerialGPS2 RelPos data
+	if (SerialGPS2->available())
+	{
+		uint8_t incoming_char = SerialGPS2->read();  //Read RELPOSNED from F9P
 
-        // Just increase the byte counter for the first 3 bytes
-        if (relposnedByteCount < 4 && incoming_char == ackPacket[relposnedByteCount])
-        {
-            relposnedByteCount++;
-        }
-        else if (relposnedByteCount > 3)
-        {
-            // Real data, put the received bytes in the buffer
-            ackPacket[relposnedByteCount] = incoming_char;
-            relposnedByteCount++;
-        }
-        else
-        {
-            // Reset the counter, becaues the start sequence was broken
-            relposnedByteCount = 0;
-        }
-    }
+		// Just increase the byte counter for the first 3 bytes
+		if (relposnedByteCount < 4 && incoming_char == ackPacket[relposnedByteCount])
+		{
+			relposnedByteCount++;
+		}
+		else if (relposnedByteCount > 3)
+		{
+			// Real data, put the received bytes in the buffer
+			ackPacket[relposnedByteCount] = incoming_char;
+			relposnedByteCount++;
+		}
+		else
+		{
+			// Reset the counter, becaues the start sequence was broken
+			relposnedByteCount = 0;
+		}
+	}
 
-    // Check the message when the buffer is full
-    if (relposnedByteCount > 71)
-    {
-        if (calcChecksum())
-        {
-            //if(deBug) Serial.println("RelPos Message Recived");
-            digitalWrite(GPSRED_LED, LOW);   //Turn red GPS LED OFF (we are now in dual mode so green LED)
-            useDual = true;
-            relPosDecode();
-        }
-        relposnedByteCount = 0;
-    }
+	// Check the message when the buffer is full
+	if (relposnedByteCount > 71)
+	{
+		if (calcChecksum())
+		{
+			//if(deBug) Serial.println("RelPos Message Recived");
+			digitalWrite(GPSRED_LED, LOW);   //Turn red GPS LED OFF (we are now in dual mode so green LED)
+			useDual = true;
+			relPosDecode();
+		}
+		relposnedByteCount = 0;
+	}
 
-    //RVC BNO08x
-    if (rvc.read(&bnoData)) useBNO08xRVC = true;
+	//RVC BNO08x
+	if (rvc.read(&bnoData)) useBNO08xRVC = true;
 
-    if (useBNO08xRVC && bnoTimer > 70 && bnoTrigger)
-    {
-        bnoTrigger = false;
-        imuHandler();   //Get IMU data ready
-    }
-    
-    if (Autosteer_running) autosteerLoop();
-    else ReceiveUdp();
-    
-    //GGA timeout, turn off GPS LED's etc
-    if (GGAReadyTime > 10000) //GGA age over 10sec
-    {
-        digitalWrite(GPSRED_LED, LOW);
-        digitalWrite(GPSGREEN_LED, LOW);
-        useDual = false;
-    }
+	if (useBNO08xRVC && bnoTimer > 70 && bnoTrigger)
+	{
+		bnoTrigger = false;
+		imuHandler();   //Get IMU data ready
+	}
 
-    if (ethernetLinkCheck > 10000)
-    {
-        if (Ethernet.linkStatus() == LinkON)
-        {
-            ethernetLinkCheck = 0;
-            digitalWrite(Power_on_LED, 0);
-            digitalWrite(Ethernet_Active_LED, 1);
-        }
-        else
-        {
-            digitalWrite(Power_on_LED, 1);
-            digitalWrite(Ethernet_Active_LED, 0);
-        }
-    }
+	if (Autosteer_running) autosteerLoop();
+	else ReceiveUdp();
+
+	//GGA timeout, turn off GPS LED's etc
+	if (GGAReadyTime > 10000) //GGA age over 10sec
+	{
+		digitalWrite(GPSRED_LED, LOW);
+		digitalWrite(GPSGREEN_LED, LOW);
+		useDual = false;
+	}
+
+	if (ethernetLinkCheck > 10000)
+	{
+		if (Ethernet.linkStatus() == LinkON)
+		{
+			ethernetLinkCheck = 0;
+			digitalWrite(Power_on_LED, 0);
+			digitalWrite(Ethernet_Active_LED, 1);
+		}
+		else
+		{
+			digitalWrite(Power_on_LED, 1);
+			digitalWrite(Ethernet_Active_LED, 0);
+		}
+	}
 
 }//End Loop
 //**************************************************************************
 
 bool calcChecksum()
 {
-  CK_A = 0;
-  CK_B = 0;
+	CK_A = 0;
+	CK_B = 0;
 
-  for (int i = 2; i < 70; i++)
-  {
-    CK_A = CK_A + ackPacket[i];
-    CK_B = CK_B + CK_A;
-  }
+	for (int i = 2; i < 70; i++)
+	{
+		CK_A = CK_A + ackPacket[i];
+		CK_B = CK_B + CK_A;
+	}
 
-  return (CK_A == ackPacket[70] && CK_B == ackPacket[71]);
+	return (CK_A == ackPacket[70] && CK_B == ackPacket[71]);
 }
