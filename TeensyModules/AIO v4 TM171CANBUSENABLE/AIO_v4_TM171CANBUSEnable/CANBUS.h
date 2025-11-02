77a1930d524bd752555432e2a14a3e2cdb50a01f
lansalot <lansalot@gmail.com>
2025-11-02 16:04:42 +0000

    AIO + TM171 + CANBUSEnable

---
+++ TeensyModules/AIO v4 TM171CANBUSENABLE/AIO_v4_TM171CANBUSEnable/CANBUS.h
@@ -0,0 +1,13 @@
+#ifndef _CANBUS_h
+#define _CANBUS_h
+
+#include <FlexCAN_T4.h>
+
+extern uint8_t currentState, reading, previous, remoteSwitch, steerSwitch;
+
+void CAN_Setup();
+void CANBUS_Receive();
+
+#endif
+
+
